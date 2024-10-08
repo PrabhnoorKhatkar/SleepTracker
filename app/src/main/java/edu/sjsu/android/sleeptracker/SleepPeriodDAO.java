6035e503f98c98ca0cfdbc86d3225420d289313d
Prabhnoor Khatkar <prabhnoor.khatkar@sjsu.edu>
2024-10-07 18:25:16 -0700

    Add DAO for SleepPeriod

---
+++ app/src/main/java/edu/sjsu/android/sleeptracker/SleepPeriodDAO.java
@@ -0,0 +1,25 @@
+package edu.sjsu.android.sleeptracker;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SleepPeriodDAO {
+
+    @Insert
+    public void addData(SleepPeriod data);
+
+    @Update
+    public void updateData(SleepPeriod data);
+
+    @Delete
+    public void deleteData(SleepPeriod data);
+
+    @Query("SELECT * FROM SleepData")
+    List<SleepPeriod> getAllSleepPeriodData();
+}
